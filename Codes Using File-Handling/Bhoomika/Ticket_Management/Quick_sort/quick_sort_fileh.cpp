11f385b6b2df912b130b8828a59ba7195b097fe7
bhoomika-malagar <168760962+bhoomika-malagar@users.noreply.github.com>
2024-12-29 23:59:37 +0530

    Update quick_sort_fileh.cpp

--- Codes Using File-Handling/Bhoomika/Ticket_Management/Quick_sort/quick_sort_fileh.cpp
+++ Codes Using File-Handling/Bhoomika/Ticket_Management/Quick_sort/quick_sort_fileh.cpp
@@ -4,40 +4,40 @@
 #include <cstring>
 using namespace std;
 
-class ticket_data {
+class ticket_data 
+{
 private:
     char name[150][30];
     char ticket_number[150][15];
     char purchase_date[150][15];
     int global_count;
 
-    // Function to partition the array for QuickSort based on ticket_number
-    int Partition(int l, int r) {
+    int Partition(int l, int r) 
+{
         string pivot = ticket_number[l];
-        int i = l + 1; // Start from the next element
+        int i = l + 1; 
         int j = r;
 
-        while (true) {
-            // Move i to the right while elements are less than or equal to the pivot
-            while (i <= r && string(ticket_number[i]) <= pivot) {
+        while (true)
+            {
+            while (i <= r && string(ticket_number[i]) <= pivot) 
+            {
                 i++;
             }
 
-            // Move j to the left while elements are greater than the pivot
-            while (j >= l && string(ticket_number[j]) > pivot) {
+            while (j >= l && string(ticket_number[j]) > pivot) 
+            {
                 j--;
             }
 
             if (i >= j)
                 break;
 
-            // Swap the elements in ticket_number array
             swap(ticket_number[i], ticket_number[j]);
-            swap(name[i], name[j]);  // Swap corresponding name
-            swap(purchase_date[i], purchase_date[j]);  // Swap corresponding purchase_date
+            swap(name[i], name[j]);  
+            swap(purchase_date[i], purchase_date[j]); 
         }
 
-        // Place the pivot element in the correct position
         swap(ticket_number[l], ticket_number[j]);
         swap(name[l], name[j]);
         swap(purchase_date[l], purchase_date[j]);
@@ -45,7 +45,6 @@ private:
         return j;
     }
 
-    // QuickSort function to sort the ticket_number array
     void QuickSort(int l, int r) {
         if (l < r) {
             int s = Partition(l, r);
@@ -60,14 +59,16 @@ public:
     void welcome();
     void load_from_file();
     void display_data();
-    void sort_data();  // Sort ticket data based on ticket_number
+    void sort_data();  
 };
 
-ticket_data::ticket_data() {
+ticket_data::ticket_data()
+{
     global_count = 0;
 }
 
-void ticket_data::welcome() {
+void ticket_data::welcome() 
+{
     for (int i = 0; i < 80; i++)
         cout << "*";
     cout << "\n\n\t\t\t\tTICKET MANAGEMENT SYSTEM\n\n\n";
@@ -76,7 +77,8 @@ void ticket_data::welcome() {
     cout << "\n\n";
 }
 
-void ticket_data::load_from_file() {
+void ticket_data::load_from_file()
+{
     ifstream file("TicketData.txt");
 
     if (!file) {
@@ -84,7 +86,7 @@ void ticket_data::load_from_file() {
         return;
     }
 
-    // Correctly read the file data
+   
     while (file.getline(name[global_count], 30, ',') &&
            file.getline(ticket_number[global_count], 15, ',') &&
            file.getline(purchase_date[global_count], 15)) {
@@ -121,7 +123,7 @@ void ticket_data::display_data() {
 
 void ticket_data::sort_data() {
     if (global_count > 1) {
-        // Call the QuickSort function to sort the ticket_number array and rearrange associated data
+
         QuickSort(0, global_count - 1);
     } else {
         cout << "Not enough data to sort.\n";
@@ -137,7 +139,7 @@ int main() {
     cout << "\nBefore sorting:\n";
     engine.display_data();
 
-    // Sort the tickets by ticket number
+   
     engine.sort_data();
 
     cout << "\nAfter sorting:\n";
